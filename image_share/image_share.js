1f245dc762bf66c06c26299ea2b4b40b93d16fb6
Khanh Dao <kdao@Khanhs-MBP-2.attlocal.net>
2016-08-07 19:32:32 -0700

    Initial check in for first assignment

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -1,23 +1,48 @@
+'use strict';
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  var image_data = [{
+    img_src: 'totoro_icon_1.jpg',
+    img_alt: 'totoro icon 1'
+  }, {
+    img_src: 'totoro_icon_2.jpg',
+    img_alt: 'totoro icon 2'
+  }, {
+    img_src: 'totoro_icon_3.jpg',
+    img_alt: 'totoro icon 3'
+  }, {
+    img_src: 'totoro_icon_4.jpg',
+    img_alt: 'totoro icon 4'
+  }, {
+    img_src: 'totoro_icon_5.png',
+    img_alt: 'totoro icon 5'
+  }, {
+    img_src: 'totoro_icon_6.gif',
+    img_alt: 'totoro icon 6'
+  }, {
+    img_src: 'totoro_icon_7.gif',
+    img_alt: 'totoro icon 7'
+  }, {
+    img_src: 'totoro_icon_8.png',
+    img_alt: 'totoro icon 8'
+  }, {
+    img_src: 'totoro_icon_9.jpg',
+    img_alt: 'totoro icon 9'
+  }];
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.images.helpers({images: image_data}); //return data to template
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  //Listen to interactivity
+  Template.images.events({
+    'click .js-image': function(event) {
+      $(event.target).css("width", "50px");
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  /*Meteor.startup(function () {
     // code to run on server at startup
-  });
+  });*/
+  console.log('I am a server');
 }
